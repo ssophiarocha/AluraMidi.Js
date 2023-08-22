f34fa94d9289ec6d484b298108bbe45a4ef80ff7
Sophia Rocha <101724908+ssophiarocha@users.noreply.github.com>
2023-08-22 10:23:56 -0300

    Código Js clean e funcional

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,3 +1,34 @@
+/*uma lista e que o que tem dentro dessa lista são teclas. 
+E é possível fazer isso com o JavaScript, usando um recurso
+ da linguagem chamado referências. As referências no 
+ JavaScript devem ser criadas ou declaradas com base no valor 
+ que elas vão receber e ter que guardar.*/
+
+ function TocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    // console.log(idAudio);
+
+    tecla.onclick= function () {
+        TocaSom(idAudio);
+    }
+
+    contador = contador + 1;
+
+    console.log (contador);
+}
+
+
 /*---------------------------------------------*/
 /* desafio:como pegar o botão do HTML e chama-lo 
 dentro do Js e executar alguma ação.
@@ -11,11 +42,11 @@ Nome do ID: (#som_tecla_pom)*/
 /* Acessar a tecla juntamente à função de dar play
 à mídia*/
 
-function TocaSomPom () {
+/*function TocaSomPom () {
     document.querySelector('#som_tecla_pom').play();
 }
-/* Vai acessar o documento HTML e retornar  a classe
-selecionada*/
+Vai acessar o documento HTML e retornar  a classe
+selecionada
 document.querySelector('.tecla_pom').onclick= TocaSomPom;
 
 function TocaSomClap() {
@@ -56,4 +87,4 @@ document.querySelector('.tecla_tic').onclick= TocaSomTic;
 function TocaSomTom() {
     document.querySelector('#som_tecla_tom').play();
 }
-document.querySelector('.tecla_tom').onclick= TocaSomTom;
+document.querySelector('.tecla_tom').onclick= TocaSomTom;*/
